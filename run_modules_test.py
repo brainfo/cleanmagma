50ddb0f156a709c3ec43791a08e4f78355f51d15
brainfo <scilavisher@gmail.com>
2025-02-15 16:08:10 +0100

    support custimized header

--- run_modules_test.py
+++ run_modules_test.py
@@ -4,4 +4,4 @@ root_folder = '/mnt/data/hong/2022/DHJ1_human_obesity_placenta/data/gwas/egg-con
 ## set logging path, and logging level
 target_folder = '/mnt/storage/hong/2024/egg-consortium'
 
-mk_db.run_single_gwas(root_folder, 'EGG-GWAS-BL.txt.gz', target_folder)
+mk_db.run_single_gwas(root_folder, 'Fetal_Effect_European_meta_NG2019.txt.gz', target_folder, p = ['p'], n=['n_ownbw'], rsid=['rsid'], chromosome=['chr'], position=['pos'])
